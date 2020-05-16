a89011f416f5417a6ac4c7c99eb36f00edc80ea1
coderbot <coderbot16@gmail.com>
2020-05-16 13:52:07 -0700

    Initial commit

---
+++ src/main/java/com/patchworkmc/specialbunny/SpecialBunny.java
@@ -0,0 +1,197 @@
+package com.patchworkmc.specialbunny;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class SpecialBunny {
+	private static final int ALL = 0;
+	private static final int COREMOD = 1;
+	private static final int FABRIC = 2;
+	private static final int FORGE = 3;
+	private static final int BOTH = 4;
+	private static final int NEITHER = 5;
+	private static final int AT = 6;
+	private static final int FORGE_LEGACY = 7;
+
+	public static void main(String[] args) throws Throwable {
+		System.out.println("PatchworkMC SpecialBunny: Mass Mod Statistics");
+
+		Path holder = Paths.get("/home/coderbot/Programming/PatchworkMC/curse-indexer-may14/mods");
+		Path output = Paths.get("output-may14");
+
+		Files.createDirectories(output);
+
+		final int[] totals = new int[9];
+
+		Files.walkFileTree(holder, new SimpleFileVisitor<Path>() {
+			public FileVisitResult visitFile(Path candidate, BasicFileAttributes attributes) throws IOException {
+				Objects.requireNonNull(candidate);
+				Objects.requireNonNull(attributes);
+
+				URI uri;
+
+				try {
+					uri = new URI("jar:" + candidate.toUri().toString());
+				} catch (URISyntaxException e) {
+					throw new RuntimeException(e);
+				}
+
+				FileSystem jar;
+
+				try {
+					jar = FileSystems.newFileSystem(uri, Collections.emptyMap());
+				} catch (IOException | UnsupportedOperationException swallowed) {
+					// Probably not a valid JAR. Skip it.
+
+					return FileVisitResult.CONTINUE;
+				}
+
+				totals[ALL]++;
+
+				Path manifest = jar.getPath("/META-INF/coremods.json");
+
+				try {
+					String coremods = new String(Files.readAllBytes(manifest), StandardCharsets.UTF_8);
+
+					handleCoreMods(candidate, jar, coremods, output);
+
+					totals[COREMOD]++;
+				} catch (NoSuchFileException swallowed) {
+					// no coremods
+				}
+
+				Path atPath = jar.getPath("/META-INF/accesstransformer.cfg");
+
+				try {
+					String accesstransformers = new String(Files.readAllBytes(atPath), StandardCharsets.UTF_8);
+
+					Path modFolder = createModFolder(candidate, output);
+					Files.copy(atPath, modFolder.resolve("accesstransformer.cfg"), StandardCopyOption.REPLACE_EXISTING);
+
+					totals[AT]++;
+				} catch (NoSuchFileException swallowed) {
+					// no ATs
+				}
+
+				Path servicesPath = jar.getPath("/META-INF/services");
+
+				try {
+					scanServices(servicesPath);
+
+					// Path modFolder = createModFolder(candidate, output);
+					// Files.copy(servicesPath, modFolder.resolve("services"));
+
+					System.out.println("Mod with services: " + candidate);
+				} catch (NoSuchFileException swallowed) {
+					// no services
+				}
+
+				boolean fabric = Files.exists(jar.getPath("/fabric.mod.json"));
+				boolean forge = Files.exists(jar.getPath("/META-INF/mods.toml"));
+				boolean forgeLegacy = Files.exists(jar.getPath("/mcmod.info"));
+
+				if (fabric && forge) {
+					System.err.println("Mod had both fabric and forge? " + candidate);
+					totals[BOTH]++;
+				} else if (fabric) {
+					totals[FABRIC]++;
+				} else if (forge) {
+					totals[FORGE]++;
+				} else if(forgeLegacy) {
+					System.err.println("Some dummy published a 1.12 or below mod on 1.13+: " + candidate);
+					totals[FORGE_LEGACY]++;
+				} else {
+					System.err.println("Mod had neither fabric nor forge? " + candidate);
+					totals[NEITHER]++;
+				}
+
+				return FileVisitResult.CONTINUE;
+			}
+		});
+
+		System.out.println("Total mods: " + totals[ALL]);
+		System.out.println("Total mods with core mods: " + totals[COREMOD] + " (" + ((totals[COREMOD] * 1000 / totals[FORGE]) / 10D) + "% of Forge mods)");
+		System.out.println("Total mods with access transformers: " + totals[AT] + " (" + ((totals[AT] * 1000 / totals[FORGE]) / 10D) + "% of Forge mods)");
+
+		System.out.println("Fabric: " + totals[FABRIC] + " Forge: " + totals[FORGE] + " Both: " + totals[BOTH] + " Forge 1.12 or below: " + totals[FORGE_LEGACY] + " Neither: " + totals[NEITHER]);
+	}
+
+	private static void scanServices(Path root) throws IOException {
+		Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+			public FileVisitResult visitFile(Path candidate, BasicFileAttributes attributes) throws IOException {
+				System.out.println("Service in " + root + " " + candidate);
+
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+
+	private static Path createModFolder(Path root, Path output) throws IOException {
+		Path modFolder = output.resolve(root.getFileName().toString());
+
+		if (!Files.exists(modFolder)) {
+			Files.createDirectory(modFolder);
+		}
+
+		return modFolder;
+	}
+
+	private static void handleCoreMods(Path root, FileSystem jar, String coremods, Path output) throws IOException {
+		System.out.println("Found coremods.json in " + root);
+		System.out.println(coremods);
+
+		JsonParser parser = new JsonParser();
+
+		JsonElement element = parser.parse(coremods);
+
+		if (!(element instanceof JsonObject)) {
+			throw new RuntimeException("parsed coremods.json was not a JsonObject");
+		}
+
+		JsonObject map = (JsonObject) element;
+
+		Path modFolder = createModFolder(root, output);
+
+		for(Map.Entry<String, JsonElement> entry:  map.entrySet()) {
+			String name = entry.getKey();
+			String path = entry.getValue().getAsString();
+
+			Path coremodFile = jar.getPath(path);
+			String javascript = new String(Files.readAllBytes(coremodFile), StandardCharsets.UTF_8);
+
+			String fileName = coremodFile.getFileName().toString();
+			Path target = modFolder.resolve(fileName);
+
+			while (Files.exists(target)) {
+				System.err.println("Coremod already exists: " + target);
+
+				fileName = "-" + fileName;
+				target = modFolder.resolve(fileName);
+			}
+
+			Files.copy(coremodFile, target, StandardCopyOption.REPLACE_EXISTING);
+
+			System.out.println("Found coremod: " + name);
+			//System.out.println(javascript);
+		}
+	}
+}
